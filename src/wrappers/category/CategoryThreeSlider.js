d93925c64e5ff6a6d38763b916ac6e652852e1a3
flip64 <65002468+flip64@users.noreply.github.com>
2025-08-30 15:27:08 +0330

    Update CategoryThreeSlider.js

--- src/wrappers/category/CategoryThreeSlider.js
+++ src/wrappers/category/CategoryThreeSlider.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import clsx from "clsx"
+import { useEffect, useState } from "react";
 import Swiper, { SwiperSlide } from "../../components/swiper";
-import categoryData from "../../data/category/category-two.json";
 import CategoryThreeSingle from "../../components/category/CategoryThreeSingle.js";
 
 // swiper slider settings
@@ -21,20 +21,27 @@ const settings = {
     }
   }
 };
-
+const BASEURL= "https://backend.bazbia.ir/api/"
 const CategoryThreeSlider = ({ spaceTopClass, spaceBottomClass }) => {
+  const [categoryData, setCategoryData] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BASEURL}products/categories`)  // آدرس API
+      .then((res) => res.json())
+      .then((data) => setCategoryData(data))
+      .catch((err) => console.error("Error fetching categories:", err));
+  }, []);
+
   return (
     <div className={clsx("collections-area", spaceTopClass, spaceBottomClass)}>
       <div className="container">
         <div className="collection-wrap">
           <div className="collection-active">
-            {categoryData && (
+            {categoryData.length > 0 && (
               <Swiper options={settings}>
                 {categoryData.map((single, key) => (
                   <SwiperSlide key={key}>
-                    <CategoryThreeSingle
-                      data={single}
-                    />
+                    <CategoryThreeSingle data={single} />
                   </SwiperSlide>
                 ))}
               </Swiper>
